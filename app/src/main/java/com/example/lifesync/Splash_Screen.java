fa093d51280f472b5da981afa296723140ab101f
Kashaf Parkar <kashaf786parkar@gmail.com>
2025-10-01 11:24:11 +0530

    Splash Screen Added and Icon

---
+++ app/src/main/java/com/example/lifesync/Splash_Screen.java
@@ -0,0 +1,26 @@
+package com.example.lifesync;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIME);
+    }
+}
